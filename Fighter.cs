180ce12362af2805567707a48c5e48a0f5abd45b
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2019-10-14 10:32:47 +0200

    Fight simulator med alla filer

---
+++ Fighter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightsimV1
+{
+	class Fighter
+	{
+		public int hp = 100;
+		
+		
+		List<string> FighterName = new List<string>();
+		 public void NameDecide()
+		{
+			
+			
+			
+			Console.WriteLine("Välj ett namn på spelare A");
+			FighterName.Add(Console.ReadLine());
+			string spelareA = FighterName[0];
+
+			Console.WriteLine("Namnet på spelare A: " + FighterName[0]);
+			
+			
+			Console.WriteLine("Välj ett namn på spelare B");
+			FighterName.Add(Console.ReadLine());
+			string spelareB = FighterName[1];
+			Console.WriteLine("Namnet på spelare B: " + FighterName[1]);
+			Console.WriteLine(spelareA);
+			Console.ReadKey();
+		}
+		
+		public void FightLoop()
+		{
+			
+
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightsimV1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Fighter Names = new Fighter();
+			Names.NameDecide();
+			Fighter A = new Fighter();
+			Fighter B = new Fighter();
+			
+			Console.ReadKey();
+		}
+		
+	}
+}
