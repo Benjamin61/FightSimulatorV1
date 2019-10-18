944382c8fd681e0da5658175e0b567be0458b57c
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2019-10-18 14:50:17 +0200

    .

--- Fighter.cs
+++ Fighter.cs
@@ -9,59 +9,60 @@ namespace FightsimV1
 {
 	class Fighter
 	{
-		public string name;
+		public string name; //Strings för att lagra informationen som körs i metoderna
 		private int hp;
-		public int SetHp()
+		public int SetHp() //Metod som används för att genererar random hp till båda karaktärerna
 		{
-			Random generator = new Random();
-			hp = generator.Next(20, 41);
-			return hp;
+			Random generator = new Random(); //Random generator
+			hp = generator.Next(20, 41); //Sätter in ett random nummer mellan 20 och 40 till inten hp
+			return hp; //retunerar värdet från det som körs ovan
 		}
 
 
 		//List<string> FighterName = new List<string>();//
-		public string NameDecide()
+		public string NameDecide() //Metod för att namnge spelarna
 		{
 			
-			name = Console.ReadLine();
+			name = Console.ReadLine(); // stringen name = spelarens input
 			Console.WriteLine("Du namngav spelaren: [" + name + "]" );
 
 			
-			return name;
+			return name; //retunerar stringen
 		}
 
-		public int Attack()
+		public int Attack() //Metod som avger hur mycket damage man ska göra
 		{
 			Random generator = new Random();
-			int dmg = generator.Next(2, 7);
-			return dmg;
+			int dmg = generator.Next(2, 7); //Skapar en int som får ett värde från generatorn mellan 2 och 6
+			return dmg; //Retunerar int dmg med ett värde
 
 		}
 
-		public void Hurt(int dmg)
+		public void Hurt(int dmg) //Metod som tar hp minus dmg från tidigare metoder
 		{
-			hp = hp - dmg;
+			hp = hp - dmg; //Gör om hp till hp - dmg
 
-			if (hp < 0)
+			if (hp < 0) //Om man tar skada så att ens hp bli under 0 så kommer det bara displaya 0, man kan inte ha negativt hp
 			{
 				hp = 0;
 			}
 		}
 
-		public int GetHp()
+		public int GetHp() //Metod som skriver ut spelarens hp
 		{
 
-			Console.WriteLine(name + " har nu " +  hp + "hp.");
+			Console.WriteLine(name + " har nu " +  hp + "hp."); //Skriver ut hp man har nu
 
-			return hp;
+			return hp; //Retunerar hp
+			
 		}
 
 
-		public bool IsAlive()
+		public bool IsAlive() //En bool för att kolla så att spelaren är vid liv
 		{
-			bool enoughHP = false;
+			bool enoughHP = false; //Sätter boolen enoughHP till false som default
 
-			if (hp >= 1)
+			if (hp >= 1) //Om spelarens hp är större eller lika med 1 så är enoughHP true
 			{
 				enoughHP = true;
 			}
@@ -69,7 +70,7 @@ namespace FightsimV1
 
 
 
-			return enoughHP;
+			return enoughHP; //Retunerar boolen enoughHP
 			
 		}
 
--- Program.cs
+++ Program.cs
@@ -12,23 +12,24 @@ namespace FightsimV1
 		{
 			
 
-			Fighter A = new Fighter();
+			Fighter A = new Fighter(); //Skapar 2 fighter från classen som kallas A respektive B
 			Fighter B = new Fighter();
 			Console.Write("Ge ett namn till spelare A:");
-			string Anamn = A.NameDecide();
-			A.SetHp();
-			bool Aalive = A.IsAlive();
+			string Anamn = A.NameDecide(); //Gör en string som kallas Anamn som = Namdecide metoden för fighter A 
+			A.SetHp(); //Kör metoden sethp för Fighter A
+			bool Aalive = A.IsAlive(); //Skapar en bool som heter  Aalive = metoden IsAlive för A
 			Console.Write("Namnge spelare B:");
-			string Bnamn = B.NameDecide();
+			string Bnamn = B.NameDecide(); //Samma sak som ovan fast nu applicerars det på spelare B
 			B.SetHp();
 			bool Balive = B.IsAlive();
 			Console.ReadKey();
-			while (Balive == true && Aalive == true) 
+			while (Balive == true && Aalive == true)  //En loop som körs så länge metoden IsAlive i figter.cs är true
 			{
-				int attackA = A.Attack();
-				B.Hurt(attackA);
-				Console.WriteLine(Anamn + " Gjorde " + attackA + " Skada");
-				B.GetHp();
+				
+				int attackA = A.Attack(); //Skapar en int där information från A.Attack lagras
+				B.Hurt(attackA); //Hurt metoden körs för B där attacken från tidigare förs med i en int
+				Console.WriteLine(Anamn + " Gjorde " + attackA + " Skada"); //Skriver ut hur mycket dmg man gjorde
+				B.GetHp(); //Skriver ut fighters B hp
 				Balive = B.IsAlive();
 				Console.ReadKey();
 				int attackB = B.Attack();
@@ -37,6 +38,7 @@ namespace FightsimV1
 				A.GetHp();
 				Aalive = A.IsAlive();
 				Console.ReadKey();
+				Console.Clear();
 			}
 
 			if (Aalive == true && Balive == false)
